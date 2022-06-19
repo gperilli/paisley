7ba99befa7d1ff04497ee023e49ff08f0bcf4c9e
gperilli <garethperilli@gmail.com>
2022-06-20 00:49:55 +0900

    containers setup

--- paisley.js
+++ paisley.js
@@ -1,5 +1,5 @@
 function setLayOut() {
-    //Get screen size
+    // Get screen size
     const body = document.querySelector("body");
     let bodyWidth = body.clientWidth;
     let bodyHeight = body.clientHeight;
@@ -10,6 +10,7 @@ function setLayOut() {
     console.log(`Screen height: ${screenHeight}, screen width: ${screenWidth}`);
     console.log(`Body height: ${bodyHeight}, body width: ${bodyWidth}`);
 
+    // determine portrait or landscape
     if (screenWidth > screenHeight) {
         centralContainerSquareWidth = screenHeight;
         centralContainerXOffset = ((screenWidth - screenHeight) / 2);
@@ -22,15 +23,52 @@ function setLayOut() {
         console.log(`Portait Screen. Central container square width: ${centralContainerSquareWidth}`);
     }
 
-    let centralContainerSquareElement = `<div id="centralContainerSquare" style="position: absolute;"></div>`;
-    body.insertAdjacentHTML("beforeend", centralContainerSquareElement);
-    let centralContainerSquare = document.querySelector("#centralContainerSquare");
-    
+    // Only insert element first time
+    if (document.querySelector("#centralContainerSquare") == null) {
+        //console.log("null element detected")
+        const centralContainerSquareElement = `<div id="centralContainerSquare" class="outline" style="position: absolute;"></div>`;
+        body.insertAdjacentHTML("beforeend", centralContainerSquareElement);
+        const centralContainerSquare = document.querySelector("#centralContainerSquare");
+    } 
+
+    // update position and size of main container square
     centralContainerSquare.style.width = "".concat(centralContainerSquareWidth,"px");
     centralContainerSquare.style.height = "".concat(centralContainerSquareWidth,"px");
-    centralContainerSquare.style.backgroundColor = "#eee";
     centralContainerSquare.style.left = "".concat(centralContainerXOffset, "px");
     centralContainerSquare.style.top = "".concat(centralContainerYOffset, "px");
+
+    ////////////////////////////////////////
+    // Pattern One
+    // Pattern 1 - central square
+    patternOneBigSquareWidth = centralContainerSquareWidth / 1.5
+    patternOneBigSquareXYOffset = centralContainerSquareWidth / 6
+
+    if (document.querySelector("#patternOneBigSquare") == null) {
+        const patternOneBigSquareElement = `<div id="patternOneBigSquare" class="outline" style="position: absolute;"></div>`;
+        centralContainerSquare.insertAdjacentHTML("beforeend", patternOneBigSquareElement);
+        const patternOneBigSquare = document.querySelector("#centralContainerSquare")
+    }
+
+    // update position and size 
+    patternOneBigSquare.style.width = "".concat(patternOneBigSquareWidth,"px");
+    patternOneBigSquare.style.height = "".concat(patternOneBigSquareWidth,"px");
+    patternOneBigSquare.style.left = "".concat((patternOneBigSquareXYOffset * 2), "px");
+    patternOneBigSquare.style.top = "".concat(patternOneBigSquareXYOffset, "px");
+
+    // Pattern 1 - periphery squares
+    patternOnePeripherySquareWidth = centralContainerSquareWidth / 6
+
+    if (document.querySelector("#patternOnePeripherySquare") == null) {
+        const patternOnePeripherySquareElement = `<div id="patternOnePeripherySquare" class="outline" style="position: absolute;"></div>`;
+        centralContainerSquare.insertAdjacentHTML("beforeend", patternOnePeripherySquareElement);
+        const patternOnePeripherySquare = document.querySelector("#patternOnePeripherySquare")
+    }
+
+    // update position and size 
+    patternOnePeripherySquare.style.width = "".concat(patternOnePeripherySquareWidth,"px");
+    patternOnePeripherySquare.style.height = "".concat(patternOnePeripherySquareWidth,"px");
+    patternOnePeripherySquare.style.left = "".concat(patternOnePeripherySquareXYOffset, "px");
+    patternOnePeripherySquare.style.top = "".concat(patternOnePeripherySquareXYOffset, "px");
 }
 
 window.addEventListener( 'load', setLayOut );
