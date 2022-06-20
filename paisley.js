9d46fcb326075b74a275dd9a45b2c9ca11e6b32a
gperilli <garethperilli@gmail.com>
2022-06-20 15:18:55 +0900

    8 containers

--- paisley.js
+++ paisley.js
@@ -1,4 +1,4 @@
-function setLayOut() {
+function setLayout() {
     // Get screen size
     const body = document.querySelector("body");
     let bodyWidth = body.clientWidth;
@@ -34,12 +34,15 @@ function setLayOut() {
     // update position and size of main container square
     centralContainerSquare.style.width = "".concat(centralContainerSquareWidth,"px");
     centralContainerSquare.style.height = "".concat(centralContainerSquareWidth,"px");
-    centralContainerSquare.style.left = "".concat(centralContainerXOffset, "px");
+    centralContainerSquare.style.left = "".concat((centralContainerXOffset * 2), "px");
     centralContainerSquare.style.top = "".concat(centralContainerYOffset, "px");
 
     ////////////////////////////////////////
     // Pattern One
     // Pattern 1 - central square
+
+    
+
     patternOneBigSquareWidth = centralContainerSquareWidth / 1.5
     patternOneBigSquareXYOffset = centralContainerSquareWidth / 6
 
@@ -58,18 +61,75 @@ function setLayOut() {
     // Pattern 1 - periphery squares
     patternOnePeripherySquareWidth = centralContainerSquareWidth / 6
 
-    if (document.querySelector("#patternOnePeripherySquare") == null) {
-        const patternOnePeripherySquareElement = `<div id="patternOnePeripherySquare" class="outline" style="position: absolute;"></div>`;
-        centralContainerSquare.insertAdjacentHTML("beforeend", patternOnePeripherySquareElement);
-        const patternOnePeripherySquare = document.querySelector("#patternOnePeripherySquare")
+
+    // PeripherySquare: width, left, top, n
+    class PeripherySquare {
+        constructor(width, left, top, n) {
+            this.width = width + "px"
+            this.height = width + "px"
+            this.leftOffset = left
+            this.topOffset = top
+            this.element = `<div id="patternOnePeripherySquare-${n}" class="outline" style="position: absolute;"></div>`
+            this.elementSelector = `#patternOnePeripherySquare-${n}`
+        }
     }
 
-    // update position and size 
-    patternOnePeripherySquare.style.width = "".concat(patternOnePeripherySquareWidth,"px");
-    patternOnePeripherySquare.style.height = "".concat(patternOnePeripherySquareWidth,"px");
-    patternOnePeripherySquare.style.left = "".concat(patternOnePeripherySquareXYOffset, "px");
-    patternOnePeripherySquare.style.top = "".concat(patternOnePeripherySquareXYOffset, "px");
+    //
+    const patternOnePeripherySquareXOffset = []
+    const patternOnePeripherySquareYOffset = []
+    peripherySquares = []
+    // Generate left side containers
+    for (let i = 0; i < 3; i++) {
+        patternOnePeripherySquareXOffset[i] = Math.random() * patternOnePeripherySquareWidth
+        patternOnePeripherySquareYOffset[i] = Math.random() * patternOnePeripherySquareWidth
+
+        // PeripherySquare: width, left, top, n
+        peripherySquares[i] = new PeripherySquare(patternOnePeripherySquareWidth, patternOnePeripherySquareXOffset[i], patternOnePeripherySquareYOffset[i], i)
+        centralContainerSquare.insertAdjacentHTML("beforeend", peripherySquares[i].element);
+        // set
+        document.querySelector(peripherySquares[i].elementSelector).style.width = peripherySquares[i].width
+        document.querySelector(peripherySquares[i].elementSelector).style.height = peripherySquares[i].width
+        // set + update
+        document.querySelector(peripherySquares[i].elementSelector).style.left = peripherySquares[i].leftOffset  + "px"
+        document.querySelector(peripherySquares[i].elementSelector).style.top = ((patternOnePeripherySquareWidth * (i * 2)) + peripherySquares[i].topOffset)  + "px"
+    }
+
+    // Generate top side containers
+    topSidePeripherySquares = []
+    patternOneTopSidePeripherySquareXOffset = []
+    for (let i = 0; i < 2; i++) {
+        patternOneTopSidePeripherySquareXOffset[i] = Math.random() * patternOnePeripherySquareWidth
+        topSidePeripherySquares[i] = new PeripherySquare(patternOnePeripherySquareWidth, patternOneTopSidePeripherySquareXOffset[i], i, (3 + i))
+        centralContainerSquare.insertAdjacentHTML("beforeend", topSidePeripherySquares[i].element);
+        // set
+        document.querySelector(topSidePeripherySquares[i].elementSelector).style.width = topSidePeripherySquares[i].width
+        document.querySelector(topSidePeripherySquares[i].elementSelector).style.height = topSidePeripherySquares[i].width
+        // set + update
+        document.querySelector(topSidePeripherySquares[i].elementSelector).style.left = topSidePeripherySquares[i].leftOffset + (patternOnePeripherySquareWidth * ((i + 1) * 2))  + "px"
+    }
+    
+    // Generate bottom side containers
+    bottomSidePeripherySquares = []
+    patternOneBottomSidePeripherySquareXOffset = []
+    for (let i = 0; i < 2; i++) {
+        patternOneBottomSidePeripherySquareXOffset[i] = Math.random() * patternOnePeripherySquareWidth
+        bottomSidePeripherySquares[i] = new PeripherySquare(patternOnePeripherySquareWidth, patternOneBottomSidePeripherySquareXOffset[i], i, (5 + i))
+        centralContainerSquare.insertAdjacentHTML("beforeend", bottomSidePeripherySquares[i].element);
+        // set
+        document.querySelector(bottomSidePeripherySquares[i].elementSelector).style.width = bottomSidePeripherySquares[i].width
+        document.querySelector(bottomSidePeripherySquares[i].elementSelector).style.height = bottomSidePeripherySquares[i].width
+        // set + update
+        document.querySelector(bottomSidePeripherySquares[i].elementSelector).style.left = bottomSidePeripherySquares[i].leftOffset + (patternOnePeripherySquareWidth * ((i + 1) * 2))  + "px"
+        document.querySelector(bottomSidePeripherySquares[i].elementSelector).style.top = bottomSidePeripherySquares[i].topOffset + (patternOnePeripherySquareWidth * 5)  + "px"
+    }
+
+    
+
 }
 
-window.addEventListener( 'load', setLayOut );
-window.addEventListener( 'resize', setLayOut );
+function updateLayout() {
+
+}
+
+window.addEventListener( 'load', setLayout );
+window.addEventListener( 'resize', updateLayout );
